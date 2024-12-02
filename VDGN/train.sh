34d4d7c77a70a1f41d8b67ffd35a7345013ca9d2
sh9q28 <19375326@buaa.edu.cn>
2024-12-02 11:37:03 +0800

    Add files via upload

---
+++ VDGN/train.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+CONFIG="VDGN/VesselGen/Options/vessel_gen_former.yml"
+CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
